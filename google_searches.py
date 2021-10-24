15a91e24bdc96bdeb6dc0052d6f0fb8c6044c231
George Iniatis <2329642I@student.gla.ac.uk>
2021-10-24 22:45:31 +0100

    Script for Google Searches updated

--- google_searches.py
+++ google_searches.py
@@ -3,8 +3,21 @@ from googlesearch import search
 from urllib.parse import urlparse
 import re
 
-# search(query, tld='com', lang='en', num=10, start=0, stop=None, pause=2.0)
-query = "\"does not cross the blood-brain barrier\""
+'''
+Might need a VPN service to run. Connecting to Marseille seems to be working really well for me
+HTTP Error 429: Too Many Requests is extremely common
+'''
+
+
+def raw_string(string):
+    return fr"{string}"
+
+
+queries = [["\"does not cross the blood-brain barrier\"", '\w+ does not cross the \w+.\w+.\w+'],
+           ["\"does not cross the BBB\"", '\w+ does not cross the BBB'],
+           ["\"doesn't cross the blood-brain barrier\"", '\w+ doesn\'t cross the \w+.\w+.\w+'],
+           ["\"doesn't cross the BBB\"", '\w+ does\'t cross the BBB']]
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
 }
@@ -24,24 +37,38 @@ domain_white_list = ['pubmed.ncbi.nlm.nih.gov', 'diabetes.diabetesjournals.org',
                      'www.apdaparkinson.org', 'www.bbrfoundation.org', 'www.rockefeller.edu', 'www.academia.edu',
                      'en.wikipedia.org', 'www.cureus.com', 'uofuhealth.utah.edu']
 
-for url in search(query, tld="com", lang='en', num=1000, stop=1000, pause=2):
-    domain = urlparse(url).netloc
-    if domain in domain_white_list:
-        request = requests.get(url, headers=headers, timeout=10)
-        if request.status_code == 200:
-            page_content = request.text
-            matches = re.findall(r'\w+ does not cross the \w+.\w+.\w+', page_content)
+# Causing memory errors
+domain_black_list = ['nursing.ceconnection.com']
 
-            for match in matches:
-                potential_drug_name = match.split(' ')[0].lower()
-                potential_drugs[potential_drug_name] = [match, url]
+index = 0
+for query in queries:
+    for url in search(query[0], tld="com", lang='en', num=100, start=0, stop=None, pause=2.0):
+        domain = urlparse(url).netloc
+        print(domain)
+        if domain not in domain_black_list:
+            try:
+                request = requests.get(url, headers=headers, timeout=10)
+                if request.status_code == 200:
+                    page_content = request.text
+                    matches = re.findall(raw_string(query[1]), page_content)
+
+                    for match in matches:
+                        potential_drug_name = match.split(' ')[0].lower()
+                        potential_drugs[potential_drug_name] = [query[0], query[1], match, url]
+            except requests.exceptions.RequestException as e:
+                print(e)
+                continue
+
+        print(index)
+        index += 1
 
 data = []
 for key, value in potential_drugs.items():
     pubchem_cid_smiles = get_pubchem_cid_and_smiles_using_name(key)
-    print(pubchem_cid_smiles)
-    if (pubchem_cid_smiles is not None) and (len(pubchem_cid_smiles) == 2):
-        data.append([pubchem_cid_smiles[1], pubchem_cid_smiles[0], key, value[0], value[1], 0])
+    if pubchem_cid_smiles is not None:
+        data.append([pubchem_cid_smiles[1], pubchem_cid_smiles[0], key, value[0], value[1], value[2], value[3], 0])
 
-dataframe = pd.DataFrame(data, columns=['SMILES', 'PubChem_CID', 'Name', 'Matched_String', 'URL', 'Class'])
+dataframe = pd.DataFrame(data,
+                         columns=['SMILES', 'PubChem_CID', 'Name', 'Query', 'Regular_Expression', 'Matched_String',
+                                  'URL', 'Class'])
 load_to_excel(dataframe, 'Google_Searches.xlsx')
